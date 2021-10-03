eef4cced01a44dea2e0ae94b08845ecdc14f8d54
roguib <roger.guib@gmail.com>
2021-10-03 13:38:46 +0200

    fix broken tests in ubuntu

--- tests/reader.test.ts
+++ tests/reader.test.ts
@@ -1,30 +1,24 @@
 import * as reader from "../src/reader";
 const path = require("path");
+const fs = require("fs");
 
 test("Inspects /tests/fixtures/ts-files with reader function", () => {
   expect(reader.reader(path.join(path.posix.resolve(), "tests/fixtures/ts-files"))).toStrictEqual([
     {
       type: "CLASS",
       filePath: path.join(path.posix.resolve(), "tests/fixtures/ts-files/foo.ts"),
-      fileData:
-        'import { Input } from "@angular/core";\r\n' +
-        "\r\n" +
-        "export class BaseComponent {\r\n" +
-        '  @Input() baseInput: "type1" | "type2" | "type3";\r\n' +
-        "}\r\n",
+      fileData: fs.readFileSync(path.join(path.posix.resolve(), "tests/fixtures/ts-files/foo.ts"), {
+        encoding: "utf8",
+        widthFileTypes: true,
+      }),
     },
     {
       type: "COMPONENT",
       filePath: path.join(path.posix.resolve(), "tests/fixtures/ts-files/var.ts"),
-      fileData:
-        'import { Input } from "@angular/core";\r\n' +
-        "@Component({\r\n" +
-        '  selector: "app-main",\r\n' +
-        '  templateUrl: "./app.main.component.html",\r\n' +
-        "})\r\n" +
-        "export class BaseComponent {\r\n" +
-        '  @Input() baseInput: "type1" | "type2" | "type3";\r\n' +
-        "}\r\n",
+      fileData: fs.readFileSync(path.join(path.posix.resolve(), "tests/fixtures/ts-files/var.ts"), {
+        encoding: "utf8",
+        withFileTypes: true,
+      }),
     },
   ]);
 });
