476a02d3d298ba4dbfbef82fc6a296372a7e259b
github-action-benchmark <github@users.noreply.github.com>
2021-10-09 15:18:25 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 26252a6ccae756dae55ce52aecc7a987a01c542f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633792595448,
+  "lastUpdate": 1633792705180,
   "repoUrl": "https://github.com/roguib/ng-vs-snippets",
   "entries": {
     "Benchmark.js Benchmark": [
@@ -25,6 +25,33 @@ window.BENCHMARK_DATA = {
             "name": "Jest test suite",
             "value": 0.48,
             "range": "±40.01%",
+            "unit": "ops/sec",
+            "extra": "7 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "roguib",
+            "username": "roguib"
+          },
+          "committer": {
+            "name": "roguib",
+            "username": "roguib"
+          },
+          "id": "26252a6ccae756dae55ce52aecc7a987a01c542f",
+          "message": "Benchmark",
+          "timestamp": "2021-10-03T14:23:21Z",
+          "url": "https://github.com/roguib/ng-vs-snippets/pull/26/commits/26252a6ccae756dae55ce52aecc7a987a01c542f"
+        },
+        "date": 1633792704384,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Jest test suite",
+            "value": 0.47,
+            "range": "±39.35%",
             "unit": "ops/sec",
             "extra": "7 samples"
           }
