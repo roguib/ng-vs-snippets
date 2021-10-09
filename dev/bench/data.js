33820710807d33b5ad454618c21d9bb7eceb0b90
github-action-benchmark <github@users.noreply.github.com>
2021-10-09 15:16:35 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for b6c8869688f1dadfac4de437023fab1aa1bdaca4

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1633792595448,
+  "repoUrl": "https://github.com/roguib/ng-vs-snippets",
+  "entries": {
+    "Benchmark.js Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "roguib",
+            "username": "roguib"
+          },
+          "committer": {
+            "name": "roguib",
+            "username": "roguib"
+          },
+          "id": "b6c8869688f1dadfac4de437023fab1aa1bdaca4",
+          "message": "Benchmark",
+          "timestamp": "2021-10-03T14:23:21Z",
+          "url": "https://github.com/roguib/ng-vs-snippets/pull/26/commits/b6c8869688f1dadfac4de437023fab1aa1bdaca4"
+        },
+        "date": 1633792594838,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Jest test suite",
+            "value": 0.48,
+            "range": "±40.01%",
+            "unit": "ops/sec",
+            "extra": "7 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
