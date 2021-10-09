c01cd5af9fe22cf396f2409b31ce8e82d9ab8885
roguib <roger.guib@gmail.com>
2021-10-09 17:06:44 +0200

    benchmark.js with jest (it has been challenging to make both packages work together)

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    testEnvironment: "node",
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-    testPathIgnorePatterns: ["/lib/", "/node_modules/", "/out/", "/build/", "/coverage/" ],
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    collectCoverage: true,
+  testEnvironment: "node",
+  reporters: ["jest-standard-reporter"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  testPathIgnorePatterns: ["/lib/", "/node_modules/", "/out/", "/build/", "/coverage/"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true,
 };
--- tests/benchmarks/benchmarks.ts
+++ tests/benchmarks/benchmarks.ts
@@ -1,31 +1,19 @@
 const Benchmark = require("benchmark");
 const suite = new Benchmark.Suite();
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
-
-const execute = (command: string): Promise<{ stdout: string; stderr; string }> => {
-  return exec(command);
-};
-
-function fib(n) {
-  return n < 2 ? n : fib(n - 1) + fib(n - 2);
-}
 
 suite
-  .add("testing workflow", () => {
-    fib(10);
-  })
-  .on("cycle", (event) => {
-    // Output benchmark result by converting benchmark result to string
-    console.log(String(event.target));
-  })
-  .run();
+  .add("foo", {
+    defer: true,
+    fn: (deferred) => {
+      var spawn = require("child_process").spawn,
+        ls = spawn("npm run test", [""], { shell: true });
 
-suite
-  .add("Benchmark test suite", async () => {
-    await execute("npm run test");
+      ls.on("exit", () => {
+        deferred.resolve();
+      });
+    },
   })
-  .on("cycle", (event) => {
+  .on("complete", (event) => {
     console.log(String(event.target));
   })
   .run();
