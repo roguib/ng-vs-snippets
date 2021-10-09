f316c93f4be5d01c515421a2e6d85e29d2d9fcaf
github-action-benchmark <github@users.noreply.github.com>
2021-10-09 07:40:31 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 9ee6eb9c7b6845c51a08f034dea082b6b4dde92c

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1633765231345,
+  "repoUrl": "https://github.com/roguib/ng-vs-snippets",
+  "entries": {
+    "Benchmark.js Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "roger.guib@gmail.com",
+            "name": "roguib",
+            "username": "roguib"
+          },
+          "committer": {
+            "email": "roger.guib@gmail.com",
+            "name": "roguib",
+            "username": "roguib"
+          },
+          "distinct": true,
+          "id": "9ee6eb9c7b6845c51a08f034dea082b6b4dde92c",
+          "message": "trigger benchmark",
+          "timestamp": "2021-10-09T09:39:46+02:00",
+          "tree_id": "0e52b573b4c6a5d2b937409b1c925956d8918a47",
+          "url": "https://github.com/roguib/ng-vs-snippets/commit/9ee6eb9c7b6845c51a08f034dea082b6b4dde92c"
+        },
+        "date": 1633765230470,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "testing workflow",
+            "value": 1015519,
+            "range": "±0.61%",
+            "unit": "ops/sec",
+            "extra": "85 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
